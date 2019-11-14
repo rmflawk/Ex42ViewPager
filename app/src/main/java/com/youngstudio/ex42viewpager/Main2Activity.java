2b24849fe3fe173b94c6a45535d382033fa252f6
rmflawk <ant921111@naver.com>
2019-11-14 17:36:25 +0900

    1

---
+++ app/src/main/java/com/youngstudio/ex42viewpager/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.youngstudio.ex42viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/youngstudio/ex42viewpager/MainActivity.java
@@ -0,0 +1,72 @@
+package com.youngstudio.ex42viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    MyAdapter adapter;
+
+    //1.대량의 데이터들
+    ArrayList<Integer> datas= new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //대량의 데이터들 추가
+        datas.add( R.drawable.gametitle_01 );
+        datas.add( R.drawable.gametitle_02 );
+        datas.add( R.drawable.gametitle_03 );
+        datas.add( R.drawable.gametitle_04 );
+        datas.add( R.drawable.gametitle_05 );
+        datas.add( R.drawable.gametitle_06 );
+        datas.add( R.drawable.gametitle_07 );
+        datas.add( R.drawable.gametitle_08 );
+        datas.add( R.drawable.gametitle_09 );
+        datas.add( R.drawable.gametitle_10 );
+
+        pager= findViewById(R.id.pager);
+        adapter= new MyAdapter(datas, getLayoutInflater());
+        pager.setAdapter(adapter);
+
+        //page를 조금이라도 움직일때마다
+        //자동으로 발동하는 메소드를 보유한 리스너를 추가
+        pager.setPageTransformer(false, new ViewPager.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                // 현재번호 0번 0.0시작 다음번호 1번 1.0시작 -0.1씩 감소 자동으로 3번씩호출
+                page.setRotationY(position*90);
+                page.setScaleX( (1- Math.abs(position))/2+0.5f); // 무조건 양수로
+                page.setScaleY( (1- Math.abs(position))/2+0.5f); // 무조건 양수로
+                page.setAlpha(1- Math.abs(position));
+            }
+        });
+    }
+
+    public void clickPrev(View view) {
+        //현재 pager가 보여주는 페이지 index
+        int index= pager.getCurrentItem();
+
+        //특정페이지로 이동(이전페이지 : 현재페이지 -1)
+        pager.setCurrentItem(index-1,true);
+    }
+
+
+    public void clickNext(View view) {
+        int index= pager.getCurrentItem();
+
+        pager.setCurrentItem(index+1,true);
+
+    }
+
+}
